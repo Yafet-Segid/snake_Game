a49bb18f00c4842988e724d666b6fb32650fab02
Yafet Segid <yafetsegid@gmail.com>
2021-07-20 03:10:33 -0500

    snake eat apple

--- app.js
+++ app.js
@@ -1,58 +1,112 @@
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-  this.total = 0;
-  this.tail = [];
+let border = document.getElementById("gameCanvas");
+let canvasContext = border.getContext("2d");
 
-  this.eat = function (pos) {
-    let d = dist(this.x, this.y, pos.x, pos.y);
-    if (d < 1) {
-      this.total++;
-      return true;
-    } else {
-      return false;
-    }
+let snake = {
+  body: [{ x: 100, y: 100 }],
+};
+
+window.onload = function () {
+  setInterval(callBoth, 800 / 3);
+
+  function callBoth() {
+    circleSnake();
+    movingAdd();
+    drawSnake();
+    moveSnake();
+    changeSnakeDirection();
+    boarderGameOver();
+    drawApple();
+    eatApple();
+  }
+};
+
+function changeSnakeDirection() {
+  document.onkeydown = function (e) {
+    var keyboard = e.key;
+
+    if (keyboard === "ArrowUp") snake.direction = "up";
+    if (keyboard === "ArrowDown") snake.direction = "down";
+    if (keyboard === "ArrowRight") snake.direction = "right";
+    if (keyboard === "ArrowLeft") snake.direction = "left";
   };
+}
 
-  this.dir = function (x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
-  };
+function moveSnake() {
+  switch (snake.direction) {
+    case "up":
+      snake.body[0].y -= 20;
+      break;
+    case "down":
+      snake.body[0].y += 20;
+      break;
+    case "right":
+      snake.body[0].x += 20;
+      break;
+    case "left":
+      snake.body[0].x -= 20;
+      break;
+  }
+}
 
-  this.death = function () {
-    for (let i = 0; i < this.tail.length; i++) {
-      let pos = this.tail[i];
-      let d = dist(this.x, this.y, pos.x, pos.y);
-      if (d < 1) {
-        console.log("starting over");
-        this.total = 0;
-        this.tail = [];
-      }
-    }
-  };
+// Clear board
 
-  this.update = function () {
-    for (let i = 0; i < this.tail.length - 1; i++) {
-      this.tail[i] = this.tail[i + 1];
-    }
-    if (this.total >= 1) {
-      this.tail[this.total - 1] = createVector(this.x, this.y);
-    }
+function movingAdd() {
+  gameBorder(0, 0, border.height, border.width, "black");
+}
+//
+function gameBorder(top, bottom, height, width, borderColor) {
+  canvasContext.fillStyle = borderColor;
+  canvasContext.fillRect(top, bottom, height, width);
+  canvasContext.beginPath();
+}
 
-    this.x = this.x + this.xspeed * scl;
-    this.y = this.y + this.yspeed * scl;
+// Game over on border text template
 
-    this.x = constrain(this.x, 0, width - scl);
-    this.y = constrain(this.y, 0, height - scl);
-  };
+function drawSnake() {
+  let snakeWidth = 20;
+  let snakeHeight = 20;
+  snake.body.forEach((main) => {
+    gameBorder(main.x, main.y, snakeWidth, snakeHeight, "lime");
+  });
+}
 
-  this.show = function () {
-    fill(255);
-    for (let i = 0; i < this.tail.length; i++) {
-      rect(this.tail[i].x, this.tail[i].y, scl, scl);
-    }
-    rect(this.x, this.y, scl, scl);
-  };
+// make snake circle
+function circleSnake(centerX, centerY, radius, borderColor) {
+  canvasContext.fillStyle = borderColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
+
+// Game Over red image
+function boarderGameOver() {
+  if (snake.body[0].x > border.width || snake.body[0].x < 0) {
+    let img = new Image();
+    img.src = "gameover.png";
+    canvasContext.drawImage(img, 210, 210, 350, 350);
+  }
+  if (snake.body[0].y > border.height || snake.body[0].y < 0) {
+    let img = new Image();
+    img.src = "gameover.png";
+    canvasContext.drawImage(img, 210, 210, 350, 350);
+  }
+}
+//
+
+let apple = {
+  x: Math.floor((Math.random() * border.width) % 20) * 20,
+  y: Math.floor((Math.random() * border.height) % 20) * 20,
+};
+
+function drawApple() {
+  gameBorder(apple.x, apple.y, 20, 20, "red");
+}
+
+function eatApple() {
+  if (snake.body[0].x === apple.x && snake.body[0].y === apple.y) {
+    drawSnake();
+
+    let snakeTail = snake[snake.length];
+    snake.body.push({ x: snakeTail.x, y: snakeTail.y });
+  }
 }
